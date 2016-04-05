344cebc4cef06e71b234d60895b1651bfc4004d1
Tory Mance <Tory@Torys-iMac.local>
2016-04-05 01:05:50 -0400

    add responsive styles

---
+++ dropdown-toggle.js
@@ -0,0 +1,7 @@
+function showDropdown() {
+  document.getElementById('mobile-dropdown-menu').style.display = 'block';
+}
+
+function hideDropdown() {
+  document.getElementById('mobile-dropdown-menu').style.display = 'none';
+}
